12450403fa5bf42e719e13b288ec224574e60057
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-09-12 17:04:08 +0100

    Add Interface Seggregation and Depdendency Inversion principles

--- solid.rb
+++ solid.rb
@@ -74,6 +74,12 @@ class Printer2023 < Printer
     end
 end
 
+print_statement(
+    transaction1,
+    transaction2,
+    transaction3
+)
+
 # Liskov Substitution Principle
 class Response
     def request
@@ -97,4 +103,89 @@ end
 
 json_response = JSONResponse.new
 send_users(json_response)
+
+# Interface seggregation principle
+
+class Car
+    def change_gear
+    end
+end
+
+class ManualCar < Car
+
+    def change_gear
+        return switch_gear_with_gearstick
+    end
+
+    private
+    def switch_gear_with_gearstick
+        # switch gear to gear_nr
+    end
+end
+
+class AutomaticCar < Car
+    def change_gear
+        # automaticly change the gear or with paddles
+    end
+
+    private
+    def automatic_or_paddle_gear_change
+    end
+end
+
+class Vehicle < ManualCar
+    
+end
+
+class SuperCar < AutomaticCar
+
+end
+
+car = SuperCar.new
+
+car.switch_gear_with_gearstick() # => Gearstick not found error!
+
+# Dependency Inversion Principle
+# tightly coupling, 
+class PasswordHasher
+    def initialize
+        @hasher = Base64Hasher.new
+    end
+
+    def hash_password(password)
+        @hasher.hash(password)
+    end
+end
+
+# we want to use a safer hasher, we'd have to change the class or create a new PasswordHasher class
+class PasswordHasher264
+    def initialize
+        @hasher = Base264Hasher.new
+    end
+
+    def hash_password(password)
+        @hasher.hash(password)
+    end
+end
+
+# good code, we reverted the dependency on the BaseNHasher
+class NewPasswordHasher
+    def initialize(hasher = Base64Hasher.new)
+        @hasher = hasher
+    end
+
+    def hash_password(password)
+        @hasher.hash(password)
+    end
+end
+
+hasher = Base1024Hasher.new
+password_hasher = PasswordHasher.new(hasher)
+
+###
+
+hasher = Base64Hasher.new
+password_hasher = PasswordHasher(hasher)
+
+password_hasher = PasswordHasher()
 
